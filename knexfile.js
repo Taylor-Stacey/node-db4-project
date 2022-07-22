b9b0c461b89fe1f1b2bacd3ce3eeb6039cd94b0c
Taylor-Stacey <taylor88stacey@gmail.com>
2022-07-21 17:00:26 -0700

    WIP

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,19 @@
+const sharedConfig = {
+  clien: "sqlite3",
+  migrations: {directory: './data/migrations'},
+  //sqlite3
+  useNullAsDefault: true,
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+}
+
 module.exports = {
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true, // needed for sqlite
-    connection: {
-      filename: './data/recipe_book.db3',
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
+    ...sharedConfig,
+    connection: { filename: './data/cook_book.db3'},
+    seeds: { directory: './data/seeds'},
   },
-};
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './data/cook_book.test.db3'},
+  },
+}
