e07d78787af3da3923a78dead3bc5e3158c4e7e1
Taylor-Stacey <taylor88stacey@gmail.com>
2022-07-21 21:53:59 -0700

    WIP

--- data/migrations/20220722000150_initial-migration.js
+++ data/migrations/20220722000150_initial-migration.js
@@ -2,22 +2,23 @@
 exports.up = async function (knex) {
   await knex.schema
     .createTable('recipes', tbl => {
-      // tbl.varchar('recipe_name', 80).unique().notNullable();
       tbl.increments('recipe_id');
+      tbl.varchar('recipe_name', 200).unique().notNullable();
     })
     .createTable('ingredients', tbl => {
       tbl.increments('ingredient_id');
-      // tbl.varchar('ingredient_name', 20).unique().notNullable();
+      tbl.varchar('ingredient_name', 20).unique().notNullable();
+      tbl.varchar('ingredient_unit', 50)
     })
     .createTable('steps', tbl => {
       tbl.increments('step_id');
       // tbl.increments('step_number');
-      // tbl.varchar('step_instructions', 100).notNullable();
+      tbl.varchar('step_instructions', 100).notNullable();
 
     })
     .createTable('step_ingredients', tbl => {
       tbl.increments('quantity_id');
-      // tbl.varchar('quantity', 10).unique();
+      tbl.varchar('quantity', 10).unique();
     })
 };
 
